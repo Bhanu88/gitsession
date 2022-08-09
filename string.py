179386b9ff5f4d28254647e62b22cb134fcfc526
Bhanu Pratap <pat.bhanu@gmail.com>
2022-08-09 20:26:34 +0200

    ex1

---
+++ string.py
@@ -0,0 +1,7 @@
+
+
+
+name ={"fruit": "Apple","size": "Large","color": "Red"}
+price =400
+
+print(f'name of the fruit is  {name["fruit"]} and its size is {name["size"]} and colour is {name["color"]} and price is {price/5} ')
