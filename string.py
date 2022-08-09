0ccc3b0c21785b9f85fcdb59404b794342c676d3
Bhanu Pratap <pat.bhanu@gmail.com>
2022-08-09 20:33:41 +0200

    random

--- string.py
+++ string.py
@@ -2,6 +2,6 @@
 from random import random
 
 name ={"fruit": "Apple","size": "Large","color": "Red"}
-price =random()
+price =random()*2000
 
 print(f'name of the fruit is  {name["fruit"]} and its size is {name["size"]} and colour is {name["color"]} and price is {price} ')
