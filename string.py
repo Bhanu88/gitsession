c9332d72b8c970cc00922822c1d5b894416bb40f
Bhanu Pratap <pat.bhanu@gmail.com>
2022-08-09 20:32:59 +0200

    random

--- string.py
+++ string.py
@@ -1,7 +1,7 @@
 
-
+from random import random
 
 name ={"fruit": "Apple","size": "Large","color": "Red"}
-price =400
+price =random()
 
-print(f'name of the fruit is  {name["fruit"]} and its size is {name["size"]} and colour is {name["color"]} and price is {price/5} ')
+print(f'name of the fruit is  {name["fruit"]} and its size is {name["size"]} and colour is {name["color"]} and price is {price} ')
