a732b52a086aac8eadf8b2696a2a465a7e46e65d
Bhanu Pratap <pat.bhanu@gmail.com>
2022-09-25 01:54:50 +0200

    cleanup

--- string.py
+++
@@ -1,7 +0,0 @@
-
-from random import random
-
-name ={"fruit": "Apple","size": "Large","color": "Red"}
-price =random()*2000
-
-print(f'name of the fruit is  {name["fruit"]} and its size is {name["size"]} and colour is {name["color"]} and price is {price} ')
